23d7cad191a857d8c273da89a9f780b0e0073e90
kawada <vgnsl94@gmail.com>
2021-02-14 00:43:01 +0900

    google books api 連携

--- src/pages/BookSearch.vue
+++ src/pages/BookSearch.vue
@@ -1,11 +1,46 @@
 <template>
   <div>
-    booksearch
+    <input type="text" v-model="keyword">
+    <button @click="search(keyword)">検索する</button>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'BookSearch',
+  data() {
+    return {
+      keyword: '',
+      searchResult: []
+    }
+  },
+  methods: {
+    async search(keyword) {
+      this.searchResult = []
+      const baseUrl = 'https://www.googleapis.com/books/v1/volumes?'
+      const params = {
+        q: `intitle:${keyword}`,
+        maxResults: 40
+      }
+      const queryParams = new URLSearchParams(params)
+
+      const res = await fetch(baseUrl + queryParams)
+      .then(res => res.json())
+      console.log(res.items)
+      
+      for(let book of res.items) {
+        let title = book.volumeInfo.title
+        let img = book.volumeInfo.imageLinks
+        let description = book.volumeInfo.description
+
+        this.searchResult.push({
+          title: title ? title : '',
+          image: img ? img.thumbnail : '',
+          description: description ? description.slice(0, 40) : '',
+        })
+      }
+    }
+  },
 
 }
 </script>

956e61dfac509e2fa019265c5115b34098a1d317
kawada <vgnsl94@gmail.com>
2021-02-13 22:49:28 +0900

    ファイル、フォルダ整理

---
+++ src/pages/BookEdit.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    bookedit
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/BookIndex.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    bookindex
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
